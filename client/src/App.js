6d4c05b4f644dcf40a015ee78184ce0572786087
soft4750 <soft4750@gmail.com>
2019-06-23 03:07:50 +0900

    Initialize node.js Mysql Connection

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import { Paper } from '@material-ui/core';
+import { Paper, CircularProgress } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,38 +17,43 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
-  
-  
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-  },
-  {
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '나동빈',
-  'birthday': '960508',
-  'gender': '남자',
-  'job': '프로그래머'
-  },
-  {
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '이순신',
-  'birthday': '961127',
-  'gender': '남자',
-  'job': '디자이너'
-  }
-]
+
   
 class App extends React.Component {
+  
+  state = {
+    customers: '',
+    completed: 0
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  componentWillMount(){
+    clearInterval(this.timer);
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1 });
+  } 
+  
+  
   render(){
   const { classes } = this.props;
   return (
@@ -65,9 +70,15 @@ class App extends React.Component {
         </TableRow>
       </TableHead>
       <TableBody>
-        {customers.map(c => {
+        {this.state.customers ? this.state.customers.map(c => {
           return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-        })}
+        }) : 
+        <TableRow>
+          <TableCell colSpan = "6" align = "center">
+            <CircularProgress className = {classes.progress} variant = "determinate" value={this.state.completed} />
+          </TableCell>
+        </TableRow>
+        }
       </TableBody>
     </Table>
     </Paper>
